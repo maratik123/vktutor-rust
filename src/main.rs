678d1a66fd2814570c7f9ce8a75ae1979ce19b3a
Marat Bukharov <maratik@yandex-team.ru>
2022-02-23 17:21:17 +0300

    add physical device

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use vulkano::device::physical::PhysicalDevice;
 use vulkano::instance::{Instance, InstanceExtensions};
 use vulkano::Version;
 
@@ -5,5 +6,6 @@ use vktutor_rust::main_window::MainWindow;
 
 fn main() {
     let instance = Instance::new(None, Version::V1_2, &InstanceExtensions::none(), None).unwrap();
+    let physical = PhysicalDevice::enumerate(&instance).next().unwrap();
     let main_window = MainWindow::default();
 }
