aad928e181e9054cc47bb7dd8c35da6cbf22bfbe
Marat Bukharov <maratik@yandex-team.ru>
2022-02-23 19:40:21 +0300

    move device context to window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use vulkano::device::physical::{PhysicalDevice, QueueFamily};
-use vulkano::device::{Device, DeviceExtensions, Features};
 use vulkano::instance::{Instance, InstanceExtensions};
 use vulkano::Version;
 
@@ -7,22 +5,5 @@ use vktutor_rust::main_window::MainWindow;
 
 fn main() {
     let instance = Instance::new(None, Version::V1_2, &InstanceExtensions::none(), None).unwrap();
-    let physical: PhysicalDevice = PhysicalDevice::enumerate(&instance).next().unwrap();
-    let mut queue_families = physical.queue_families();
-    let queue_family = queue_families.find(|&q| q.supports_graphics()).unwrap();
-    for family in physical.queue_families() {
-        println!(
-            "Found a queue family with {:?} queue(s)",
-            family.queues_count()
-        );
-    }
-    let (device, mut queues) = Device::new(
-        physical,
-        &Features::none(),
-        &DeviceExtensions::none(),
-        [(queue_family, 0.5)].iter().cloned(),
-    )
-    .unwrap();
-    let queue = queues.next().unwrap();
-    let main_window = MainWindow::default();
+    let main_window = MainWindow::new(&instance);
 }
--- src/main_window.rs
+++ src/main_window.rs
@@ -1,7 +1,40 @@
-pub struct MainWindow;
+use std::sync::Arc;
 
-impl Default for MainWindow {
-    fn default() -> Self {
-        MainWindow
+use vulkano::device::physical::PhysicalDevice;
+use vulkano::device::{Device, DeviceExtensions, Features, Queue};
+use vulkano::instance::Instance;
+
+pub struct MainWindow<'a> {
+    instance: &'a Arc<Instance>,
+    physical_device: PhysicalDevice<'a>,
+    device: Arc<Device>,
+    queue: Arc<Queue>,
+}
+
+impl<'a> MainWindow<'a> {
+    pub fn new(instance: &'a Arc<Instance>) -> MainWindow<'a> {
+        let physical_device: PhysicalDevice = PhysicalDevice::enumerate(&instance).next().unwrap();
+        let mut queue_families = physical_device.queue_families();
+        let queue_family = queue_families.find(|&q| q.supports_graphics()).unwrap();
+        for family in physical_device.queue_families() {
+            println!(
+                "Found a queue family with {:?} queue(s)",
+                family.queues_count()
+            );
+        }
+        let (device, mut queues) = Device::new(
+            physical_device,
+            &Features::none(),
+            &DeviceExtensions::none(),
+            [(queue_family, 0.5)].iter().cloned(),
+        )
+        .unwrap();
+        let queue = queues.next().unwrap();
+        MainWindow {
+            instance,
+            physical_device,
+            device: device,
+            queue: queue,
+        }
     }
 }
