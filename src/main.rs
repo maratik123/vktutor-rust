06dd572955f297246cc48bc3b0fd085c3433aceb
Marat Bukharov <maratik@yandex-team.ru>
2022-02-23 17:28:08 +0300

    enumerate queue families

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use vulkano::device::physical::PhysicalDevice;
+use vulkano::device::physical::{PhysicalDevice, QueueFamily};
 use vulkano::instance::{Instance, InstanceExtensions};
 use vulkano::Version;
 
@@ -6,6 +6,12 @@ use vktutor_rust::main_window::MainWindow;
 
 fn main() {
     let instance = Instance::new(None, Version::V1_2, &InstanceExtensions::none(), None).unwrap();
-    let physical = PhysicalDevice::enumerate(&instance).next().unwrap();
+    let physical: PhysicalDevice = PhysicalDevice::enumerate(&instance).next().unwrap();
+    for family in physical.queue_families() {
+        println!(
+            "Found a queue family with {:?} queue(s)",
+            family.queues_count()
+        );
+    }
     let main_window = MainWindow::default();
 }
