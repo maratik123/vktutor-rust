152756e3c06f0f8e2a0f13288973d7d588c5a814
Marat Bukharov <maratik@yandex-team.ru>
2022-02-24 22:23:55 +0300

    draw triangle: find device

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,51 @@
-use vulkano::instance::{Instance, InstanceExtensions};
-use vulkano::Version;
-
-use vktutor_rust::main_window::MainWindow;
+use vulkano::device::physical::{PhysicalDevice, PhysicalDeviceType};
+use vulkano::device::DeviceExtensions;
+use vulkano::instance::Instance;
+use vulkano::{app_info_from_cargo_toml, Version};
+use vulkano_win::VkSurfaceBuild;
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
 
 fn main() {
-    let instance = Instance::new(None, Version::V1_2, &InstanceExtensions::none(), None).unwrap();
-    let main_window = MainWindow::new(&instance);
+    let required_extensions = vulkano_win::required_extensions();
+    let layers = ["VK_LAYER_KHRONOS_validation"];
+    let instance = Instance::new(
+        Some(&app_info_from_cargo_toml!()),
+        Version::V1_2,
+        &required_extensions,
+        layers,
+    )
+    .unwrap();
+
+    let event_loop = EventLoop::new();
+    let surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance.clone())
+        .unwrap();
+
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::none()
+    };
+
+    let (physical_device, queue_family) = PhysicalDevice::enumerate(&instance)
+        .filter(|&p| p.supported_extensions().is_superset_of(&device_extensions))
+        .filter_map(|p| {
+            p.queue_families()
+                .find(|&q| q.supports_graphics() && surface.is_supported(q).unwrap_or(false))
+                .map(|q| (p, q))
+        })
+        .min_by_key(|(p, _)| match p.properties().device_type {
+            PhysicalDeviceType::DiscreteGpu => 0,
+            PhysicalDeviceType::IntegratedGpu => 1,
+            PhysicalDeviceType::VirtualGpu => 2,
+            PhysicalDeviceType::Cpu => 3,
+            PhysicalDeviceType::Other => 4,
+        })
+        .unwrap();
+
+    println!(
+        "Using device: {}, (type: {:?})",
+        physical_device.properties().device_name,
+        physical_device.properties().device_type
+    );
 }
